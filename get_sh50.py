178601f621c39221af111f3551028afd0beab9f0
HollisYu <hollis_yu@outlook.com>
2019-11-21 00:34:57 +0800

    get sh50 info

--- get_sh50.py
+++ get_sh50.py
@@ -5,27 +5,34 @@
 @Author: HollisYu
 @Date: 2019-11-20 14:31:27
 @LastEditors: HollisYu
-@LastEditTime: 2019-11-20 18:51:52
+@LastEditTime: 2019-11-21 00:34:02
 '''
 import pandas as pd
 import os
 
 res = {}
-# date_data = os.listdir("F:/Programming/Dataset/his_sh1_Day/")
-date_data = os.listdir("D:/Files/Codes/Dataset/his_sh1_Day")
+date_data = os.listdir("F:/Programming/Dataset/StockInformation/his_sh1_Day/")
 for ind, d in enumerate(date_data):
     date_data[ind] = d[:-8]
     res[date_data[ind]] = []
-print(date_data)
 
-with open("F:/Programming/Dataset/cons.txt") as f:
+with open("F:/Programming/Dataset/StockInformation/index_cons.txt") as f:
+    # drop header
     line = f.readline()
     while line:
+        line = f.readline()
         info = line.split("|")
+        # only get sh_50(000016) info
+        if info[0] != "000016":
+            continue
 
+        if info[3] == " ":
+            info[3] = "20191008"
         for d in date_data:
             if d >= info[2] and d <= info[3]:
                 res[d].append(info[1])
 
-with open("F:/Programming/Dataset/sh_50.txt", 'w') as f:
-    f.write(str(res))
+for key, value in res.items():
+    with open("./sh_50/" + key + ".txt", 'w') as f:
+        print(key + " length: " + str(len(value)))
+        f.write(str(value))
