73873e1747c263441fbdae53e097c2eaf9a475fc
HollisYu <hollis_yu@outlook.com>
2019-12-19 10:17:37 +0800

    add kdj data and change macd parameters

--- cal_MACD.py
+++ cal_MACD.py
@@ -31,7 +31,7 @@ import pandas as pd
 
 #     return stock_data
 
-file_path = "./sh1_each_stock_data-bak/"
+file_path = "./sh1_each_stock_data/"
 result_path = "./sh1_each_stock_data2/"
 if not os.path.exists(result_path):
     os.mkdir(result_path)
@@ -40,10 +40,10 @@ files = os.listdir(file_path)
 for f in files:
     stock_data = pd.read_csv(file_path + f)
     # cal MACD
-    stock_data['EMA12'] = stock_data['LastPx'].ewm(span=12).mean()
-    stock_data['EMA26'] = stock_data['LastPx'].ewm(span=26).mean()
+    stock_data['EMA12'] = stock_data['LastPx'].ewm(span=6).mean()
+    stock_data['EMA26'] = stock_data['LastPx'].ewm(span=30).mean()
     stock_data['DIF'] = stock_data['EMA12'] - stock_data['EMA26']
-    stock_data['DEA'] = stock_data['DIF'].ewm(span=9).mean()
+    stock_data['DEA'] = stock_data['DIF'].ewm(span=6).mean()
     stock_data['MACD'] = (stock_data['DIF'] - stock_data['DEA']) * 2
 
     # cal KDJ
